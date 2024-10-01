4f211e613681104ba15359e91a44588bb350eeac
caioo <matossousacaio.cms@gmail.com>
2024-10-01 19:52:09 -0300

    Costumized Exceptions
    Created costumized exceptions handler
    
    UnsupportedMathOperationException

--- 05_first_steps/src/main/java/com/estudos/__first_steps/controllers/MathController.java
+++ 05_first_steps/src/main/java/com/estudos/__first_steps/controllers/MathController.java
@@ -3,6 +3,9 @@ package com.estudos.__first_steps.controllers;
 // import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.estudos.__first_steps.exceptions.UnsupportedMathOperationException;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,8 +19,8 @@ public class MathController {
     public Double sum(@PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 
-        if (!IsNumeric(numberOne) || !IsNumeric(numberTwo)) {
-            throw new Exception();
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please, set a numeric value");
         }
 
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
@@ -27,12 +30,12 @@ public class MathController {
         if (strNumber == null)
             return 0D;
         String number = strNumber.replaceAll(",", ".");
-        if (IsNumeric(number))
+        if (isNumeric(number))
             return Double.parseDouble(number);
         return 0D;
     }
 
-    private boolean IsNumeric(String strNumber) {
+    private boolean isNumeric(String strNumber) {
         if (strNumber == null) return false;
         String number = strNumber.replaceAll(",", ".");
 
---
+++ 05_first_steps/src/main/java/com/estudos/__first_steps/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.estudos.__first_steps.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.estudos.__first_steps.exceptions.ExceptionResponse;
+import com.estudos.__first_steps.exceptions.UnsupportedMathOperationException;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    //método para exceções mais genéricas 
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest wRequest) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+            new Date(), 
+            ex.getMessage(),
+            wRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //Método para exceção específica para operações não suportadas, como por exemplo passar uma letra no parâmetro ao invés de um número
+    @ExceptionHandler(UnsupportedMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest wRequest) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+            new Date(), 
+            ex.getMessage(),
+            wRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
