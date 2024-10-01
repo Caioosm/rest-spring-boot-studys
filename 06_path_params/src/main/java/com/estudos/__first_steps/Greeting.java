334eab26e182133962a7583b794bf7191a64ca17
caioo <matossousacaio.cms@gmail.com>
2024-10-01 18:33:15 -0300

    files initials

---
+++ 06_path_params/src/main/java/com/estudos/__first_steps/Greeting.java
@@ -0,0 +1,12 @@
+package com.estudos.__first_steps;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor 
+public class Greeting {
+
+    private final Long id;
+    private final String content;
+}
