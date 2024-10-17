c9139a971145e69ccb29f9d9890442ffdfd89d21
caioo <matossousacaio.cms@gmail.com>
2024-10-16 22:32:41 -0300

    web config

--- 05_first_steps/src/main/java/com/estudos/__first_steps/controllers/PersonController.java
+++ 05_first_steps/src/main/java/com/estudos/__first_steps/controllers/PersonController.java
@@ -27,31 +27,33 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    //request method for find person by id
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    // request method for find person by id
+    @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public Person findById(@PathVariable(value = "id") Long id) {
         return personService.findById(id);
     }
 
-    //request method for find all persons
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    // request method for find all persons
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public List<Person> findAll() {
         return personService.findAll();
     }
 
-    //request method for create a one persons
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    // request method for create a one persons
+    @PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, consumes = {
+            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public Person create(@RequestBody Person person) {
         return personService.create(person);
     }
-    
-    //Request method for update a one person
-    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+
+    // Request method for update a one person
+    @PutMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, consumes = {
+            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public Person update(@RequestBody Person person) {
         return personService.update(person);
     }
 
-    //request method for find person by id
+    // request method for find person by id
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         personService.delete(id);
